1d3fd4f65eead6b291ebd0f99cfe5e1b438e0ea5
tagty <bouzu.ao@gmail.com>
2021-05-06 09:04:16 +0900

    add: update list

--- app/presentations/mutations/updatePresentation.ts
+++ app/presentations/mutations/updatePresentation.ts
@@ -15,34 +15,54 @@ export default resolver.pipe(
   resolver.authorize(),
   async ({ id, ...data }) => {
     // TODO: in multi-tenant app, you must add validation to ensure correct tenant
+
+    // Update
     const presentation = await db.presentation.update({ where: { id }, data })
 
     // Delete
+    // TODO: get slides
     const slideDelete = await db.slide.findFirst({
       where: {
         presentationId: data.id,
       },
+      orderBy: {
+        id: "desc",
+      },
     })
 
-    const slideDeleteId = slideDelete?.id
+    const slideIdDelete = slideDelete?.id
+
+    console.log(slideIdDelete)
 
     const blocksDelete = await db.block.findMany({
       where: {
-        slideId: slideDeleteId,
+        slideId: slideIdDelete,
       },
     })
 
     const buildableIds = blocksDelete.map((block) => block.buildableId)
 
+    console.log(buildableIds)
+
+    // [{type: '', id: ''}, {type: '', id: ''}]
+    const buildables = blocksDelete.map((block) => {
+      return {
+        type: block.buildableType,
+        id: block.id,
+      }
+    })
+
+    console.log(buildables)
+
     const deleteBlocks = db.block.deleteMany({
       where: {
-        slideId: slideDeleteId,
+        slideId: slideIdDelete,
       },
     })
 
     const deleteSlide = db.slide.delete({
       where: {
-        id: slideDeleteId,
+        id: slideIdDelete,
       },
     })
 
@@ -64,22 +84,62 @@ export default resolver.pipe(
       },
     })
 
-    const text = data.text.replace("# ", "")
+    // const text = data.text.replace("# ", "")
 
-    const blockH1 = await db.blockH1.create({
-      data: {
-        text: text,
-      },
-    })
+    // const blockH1 = await db.blockH1.create({
+    //   data: {
+    //     text: text,
+    //   },
+    // })
 
-    await db.block.create({
-      data: {
-        text: data.text,
-        buildableId: blockH1.id,
-        buildableType: "BlockH1",
-        slideId: slide.id,
-      },
-    })
+    // await db.block.create({
+    //   data: {
+    //     text: data.text,
+    //     buildableId: blockH1.id,
+    //     buildableType: "BlockH1",
+    //     slideId: slide.id,
+    //   },
+    // })
+
+    const rows = data.text.split("\n")
+
+    for (let row of rows) {
+      if (row.startsWith("# ")) {
+        const text = row.replace("# ", "")
+
+        const buildable = await db.blockH1.create({
+          data: {
+            text: text,
+          },
+        })
+
+        await db.block.create({
+          data: {
+            text: data.text,
+            buildableId: buildable.id,
+            buildableType: "BlockH1",
+            slideId: slide.id,
+          },
+        })
+      } else if (row.startsWith("- ")) {
+        const text = row.replace("- ", "")
+
+        const buildable = await db.blockList.create({
+          data: {
+            text: text,
+          },
+        })
+
+        await db.block.create({
+          data: {
+            text: data.text,
+            buildableId: buildable.id,
+            buildableType: "BlockList",
+            slideId: slide.id,
+          },
+        })
+      }
+    }
 
     return presentation
   }
